520f611125e50193a3cd4aeebef0dee3c5398c17
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2023-02-12 00:11:55 +0300

    Add project files.

---
+++ Entities/Concrete/Role.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entittes.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Role:EntityBase,IEntity
+    {
+        public string Name { get; set; }
+        public ICollection<User> Users { get; set; }
+    }
+}
