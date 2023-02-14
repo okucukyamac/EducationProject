7e90656c16dc7267ae69a3feec290e4407190271
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2023-02-14 23:52:18 +0300

    cmt

--- Entities/Dtos/CategoryAddDto.cs
+++ Entities/Dtos/CategoryAddDto.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,11 @@ namespace Entities.Dtos
 {
     public class CategoryAddDto
     {
+        [DisplayName("Kategori Adı")]
+        [Required(ErrorMessage ="Kategori Adı boş geçilemez.")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Note { get; set; }
+        public bool IsActive { get; set; }
     }
 }
