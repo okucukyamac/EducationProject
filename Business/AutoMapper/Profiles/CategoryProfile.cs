b2eed2a9eddf1de7c4b293949dac33f8f0370f0c
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2023-02-16 01:02:30 +0300

    automapper category and article

---
+++ Business/AutoMapper/Profiles/CategoryProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Entities.Concrete;
+using Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.AutoMapper.Profiles
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<CategoryAddDto, Category>().ForMember(dest => dest.InsertDate, opt => opt.MapFrom(x => DateTime.Now));
+            CreateMap<CategoryUpdateDto, Category>().ForMember(dest => dest.InsertDate, opt => opt.MapFrom(x => DateTime.Now));
+        }
+
+    }
+}
